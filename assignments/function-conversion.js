942885dc74c6de473301f87bb7b3401bcaf7bd52
ParrishJ <jarparrish@gmail.com>
2019-04-01 15:41:33 -0700

    Finished assignment with stretch goals

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -18,8 +18,6 @@ let subtract = (param1, param2) =>
 
 // Stretch
 
-// exampleArray = [1,2,3,4];
-// const triple = exampleArray.map(function (num) {
-//   return num * 3;
-// });
-// console.log(triple);
+const exampleArray = [1,2,3,4];
+    const triple = exampleArray.map(i => i * 3) 
+    console.log(triple);
